c465b5852285f0f752133515849e044881a1574f
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-07-18 08:34:04 +0200

    change field type

--- src/udala/zinema/content/pelikula_container.py
+++ src/udala/zinema/content/pelikula_container.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from plone.app.textfield import RichText
+
 # from plone.autoform import directives
 from plone.dexterity.content import Container
 
@@ -16,36 +17,61 @@ from udala.zinema import _
 
 
 class IPelikulaContainer(model.Schema):
-    """ Marker interface and Dexterity Python Schema for PelikulaContainer
-    """
-
+    """Marker interface and Dexterity Python Schema for PelikulaContainer"""
 
     initial_stuff = RichText(
-        title=_(u'Hitzorduaren hasierako testua'),
-        description=_('Testu orokor hau ebentuaren aurretik agertuko da eta bere helburua webgunearen maketazio txukun bat edukitzea da. Informazio orokorra dago bertan, hala nola, txartel salmentaren ordutegia eta informazio gehiago lortzeko telefonoa.'),
-        required=False
+        title=_("Hitzorduaren hasierako testua"),
+        description=_(
+            "Testu orokor hau ebentuaren aurretik agertuko da eta bere helburua webgunearen maketazio txukun bat edukitzea da. Informazio orokorra dago bertan, hala nola, txartel salmentaren ordutegia eta informazio gehiago lortzeko telefonoa."
+        ),
+        required=False,
     )
 
-    text = RichText(
-        title=_(u'Programazioaren informazioa'),
-        required=False
+    text = RichText(title=_("Programazioaren informazioa"), required=False)
+
+    title_eu = schema.TextLine(title=_("Sortuko duen hitzorduaren titulua euskaraz"))
+
+    title_es = schema.TextLine(title=_("Sortuko duen hitzorduaren titulua gazteleraz"))
+
+    # subject_eu = schema.List(title=_(u"Sortuko duen hitzorduaren titulua euskaraz"))
+
+    # subject_es = schema.List(title=_(u"Sortuko duen hitzorduaren titulua gazteleraz"))
+
+    subject_eu = schema.List(
+        title=_(
+            "Sortuko den hitzorduaren etiketak euskaraz",
+        ),
+        description=_(
+            "",
+        ),
+        value_type=schema.TextLine(
+            title="",
+        ),
+        default=[],
+        required=False,
+        readonly=False,
     )
 
-    title_eu = schema.TextLine(title=_(u"Sortuko duen hitzorduaren titulua euskaraz"))
+    subject_es = schema.List(
+        title=_(
+            "Sortuko den hitzorduaren etiketak gazteleraz",
+        ),
+        description=_(
+            "",
+        ),
+        value_type=schema.TextLine(
+            title="",
+        ),
+        default=[],
+        required=False,
+        readonly=False,
+    )
 
-    title_es = schema.TextLine(title=_(u"Sortuko duen hitzorduaren titulua gazteleraz"))
+    location_eu = schema.TextLine(title=_("Lekua euskaraz"))
 
-    subject_eu = schema.List(title=_(u"Sortuko duen hitzorduaren titulua euskaraz"))
-
-    subject_es = schema.List(title=_(u"Sortuko duen hitzorduaren titulua gazteleraz"))
-
-    location_eu = schema.TextLine(title=_(u"Lekua euskaraz"))
-
-    location_es = schema.TextLine(title=_(u"Lekua gazteleraz"))
-
+    location_es = schema.TextLine(title=_("Lekua gazteleraz"))
 
 
 @implementer(IPelikulaContainer)
 class PelikulaContainer(Container):
-    """ Content-type class for IPelikulaContainer
-    """
+    """Content-type class for IPelikulaContainer"""
