26eab22e48861b84edf167bdd75aa1d787478a11
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-09-26 12:52:20 +0200

    language independent

--- src/udala/zinema/content/pelikula.py
+++ src/udala/zinema/content/pelikula.py
@@ -20,50 +20,58 @@ from DateTime import DateTime
 from plone.base.i18nl10n import weekdayname_msgid
 from zope.i18n import translate
 
+
 class ISaioaRowSchema(Interface):
-    eguna = schema.TextLine(title=_(u"Eguna"), default="AAAA/MM/DD")
-    ordua = schema.TextLine(title=_(u"Ordua"), default="HH:MM")
-    aretoa = schema.TextLine(title=_(u"Aretoa"))
+    eguna = schema.TextLine(title=_("Eguna"), default="AAAA/MM/DD")
+    ordua = schema.TextLine(title=_("Ordua"), default="HH:MM")
+    aretoa = schema.TextLine(title=_("Aretoa"))
+
 
 class IPelikula(model.Schema):
-    """ Marker interface and Dexterity Python Schema for Pelikula
-    """
+    """Marker interface and Dexterity Python Schema for Pelikula"""
+
     ficha_tecnica = RichText(
-        title=_(u'Ficha tecnica'),
+        title=_("Ficha tecnica"),
         description=_("Ficha tecnica de la pelicula en catellano"),
-        required=False
+        required=False,
     )
 
     fitxa_teknikoa = RichText(
-        title=_(u'Fitxa teknikoa'),
+        title=_("Fitxa teknikoa"),
         description=_("Pelikularen fitxa teknikoa euskaraz"),
-        required=False
+        required=False,
     )
 
     trailer = schema.Text(
-        title=_(u"Pelikularen trailerra"),
-        description=_(u"Itsatsi hemen pelikularen trailerraren embed kodea")
-        )
+        title=_("Pelikularen trailerra"),
+        description=_("Itsatsi hemen pelikularen trailerraren embed kodea"),
+    )
 
     alt_text = schema.TextLine(
-        title=u"Image alt text",
-        description=u"Enter the alternative text for the image that will be read to invident users",
+        title="Image alt text",
+        description="Enter the alternative text for the image that will be read to invident users",
         required=False,
     )
 
     widget(saioak=DataGridFieldFactory)
     saioak = schema.List(
-        title=_(u"Saioak"),
-        value_type=DictRow(title=_(u"Saioak"), schema=ISaioaRowSchema),
+        title=_("Saioak"),
+        value_type=DictRow(title=_("Saioak"), schema=ISaioaRowSchema),
         required=False,
     )
 
 
+alsoProvides(IPelikula["ficha_tecnica"], ILanguageIndependentField)
+alsoProvides(IPelikula["fitxa_teknikoa"], ILanguageIndependentField)
+alsoProvides(IPelikula["trailer"], ILanguageIndependentField)
+alsoProvides(IPelikula["alt_text"], ILanguageIndependentField)
+alsoProvides(IPelikula["saioak"], ILanguageIndependentField)
+
 
 @implementer(IPelikula)
 class Pelikula(Container):
-    """ Content-type class for IPelikula
-    """
+    """Content-type class for IPelikula"""
+
     def get_sorted_data(self):
         ret = {}
         for saioa in self.saioak:
